231f00b27b018b3deab883c908c317a22703433b
kruthikaj532-lab <kruthikaj532@gmail.com>
2026-03-29 23:01:45 -0700

    Refactor main method and retain add function

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,15 +1,13 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, Maven Pipeline!");
-    }
 
     public int add(int a, int b) {
         return a + b;
     }
 
-    public int multiply(int a, int b) {
-        return a * b;
+    public static void main(String[] args) {
+        App app = new App();
+        System.out.println("Sum: " + app.add(5, 3));
     }
 }
