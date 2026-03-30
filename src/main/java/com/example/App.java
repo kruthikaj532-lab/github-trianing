87ee4421fa031a006c06f257882988250c8d91c4
kruthikaj532-lab <kruthikaj532@gmail.com>
2026-03-29 22:56:07 -0700

    Add App class with main method and add/multiply methods

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven Pipeline!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
